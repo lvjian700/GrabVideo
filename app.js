fabdfacf3909aff80f9687a00acb4c0c899481ea
Jian Lv <jlv@thoughtworks.com>
2014-10-29 14:16:31 +0800

    add http api

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+
+var finder = require('./finders/M3U8Finder').finder;
+
+app.get('/m3u8/:url', function (req, res) {
+	var decodedURL = decodeURIComponent(req.params.url);
+	console.log('find on url:');
+	console.log(decodedURL);
+
+	console.log('find...');
+	finder.find(decodedURL, function(videoSrc) {
+		res.json({
+			m3u8: videoSrc
+		});
+	});		
+});
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
